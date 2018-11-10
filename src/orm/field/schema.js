c5f6a1383d122c0eda27c5baa8ea4a175f724954
Andrew Bettke <abettke@gmail.com>
2018-11-10 20:51:22 +1300

    Convert syntax to es6 modules.

--- src/orm/field/schema.js
+++ src/orm/field/schema.js
@@ -1,6 +1,6 @@
 import jsonSchemaDraft07 from 'ajv/lib/refs/json-schema-draft-07.json';
 
-module.exports = {
+export default {
 	$schema: 'http://json-schema.org/draft-07/schema#',
 	title: 'Chimera Field',
 	description: 'Validates data as a JSON Schema property.',
--- src/orm/model/schema.js
+++ src/orm/model/schema.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
 	$schema: 'http://json-schema.org/draft-07/schema#',
 	title: 'Chimera Model',
 	description: 'Schema document for Chimera models.',
