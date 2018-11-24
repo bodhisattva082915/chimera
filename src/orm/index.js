416f09bf49905b6822e76cb141244a8b6bd2a0eb
Andrew <andrew@Andrews-MacBook-Pro-3.local>
2018-11-24 13:38:52 +1300

    Add reverse relation virtuals to association plugin.

--- src/orm/index.js
+++ src/orm/index.js
@@ -1,10 +1,27 @@
 import fs from 'fs';
+import { each } from "lodash";
+import mongoose from "mongoose";
+
+const models = [];
 
 fs
 	.readdirSync(__dirname)
 	.filter(file => !file.includes('.js'))
 	.forEach(model => {
-		const cls = require(`${__dirname}/${model}`).default;
-
-		cls.associate();
+		models.push(require(`${__dirname}/${model}`).default);
 	});
+
+models.forEach(model => {
+	model.associate();
+});
+
+/**
+ * // TODO: Devise a more elegant approach to this.
+ * Run associations and recompile models. Mongoose only compiles models once as it is a costly
+ * operation. However, to run associations model registration must be complete to ensure reverse
+ * relations can be generated correctly. This calls for a recompilation of models after associations
+ * are run to ensure the model class has the newly applied virtuals.
+ */
+each(mongoose.models, (model, modelKey) => {
+	mongoose.models[modelKey] = model.compile(model.modelName, model.schema, model.collection.name, model.db, mongoose);
+});
