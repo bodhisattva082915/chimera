9d6500e07d93b46072247e036bf3848ac66d39ad
Andrew Bettke <abettke@gmail.com>
2018-11-30 10:01:17 +1300

    Updated ChimerField mimic typing from Mongoose Schemas.

--- test/factories/field.factory.js
+++ test/factories/field.factory.js
@@ -4,5 +4,5 @@ import ChimeraField from 'app/orm/field';
 factory.define('ChimeraField', ChimeraField, {
 	chimeraModelId: factory.assoc('ChimeraModel', '_id'),
 	name: factory.chance('word'),
-	type: 'string'
+	type: 'String'
 });
