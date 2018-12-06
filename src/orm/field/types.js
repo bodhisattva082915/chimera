6f33173891c2c3229f932b81790d2450f624116f
Andrew Bettke <abettke@gmail.com>
2018-12-06 13:00:53 +1300

    Add support for UUID schema type.

--- src/orm/field/types.js
+++ src/orm/field/types.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import isEmail from 'validator/lib/isEmail';
+import isUUID from 'validator/lib/isUUID';
 
 export class Email extends mongoose.SchemaType {
 	cast (val) {
@@ -13,6 +14,33 @@ export class Email extends mongoose.SchemaType {
 	}
 }
 
+export class UUID extends mongoose.SchemaType {
+	constructor (...args) {
+		const _opts = args[1];
+		const versions = [3, 4, 5];
+
+		if (_opts.version && !versions.includes(_opts.version)) {
+			throw new Error(`${_opts.version} is not a valid version value for SchemaType UUID.`);
+		}
+
+		super(...args);
+	}
+
+	cast (val) {
+		const _val = String(val);
+
+		if (this.options.version) {
+			if (!isUUID(_val, this.options.version)) {
+				throw new Error(`${_val} is not a valid v${this.options.version} UUID`);
+			}
+		} else {
+			if (!isUUID(_val)) {
+				throw new Error(`${_val} is not a valid UUID`);
+			}
+		}
+	}
+}
+
 /**
  * TODO: Other types to support natively
  * * Currency
@@ -20,13 +48,14 @@ export class Email extends mongoose.SchemaType {
  * * Phone
  * * IP Address
  * * URL
- * * UUID
  * * Function Fields
  */
 
 mongoose.Schema.Types = {
 	...mongoose.Schema.Types,
 
-	// Custom Chimera Types
-	Email
+	/* Custom Chimera Types */
+	Email,
+	UUID,
+	Uuid: UUID // Alias needed to support lowercase values ({ uuidField: 'uuid' })
 };
