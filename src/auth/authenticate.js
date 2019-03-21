cbf7b654fe1923488aa7843be84699287769dc3e
Andrew Bettke <14064130+abettke@users.noreply.github.com>
2019-03-21 21:18:45 +1300

    Finish basic user authentication strategy.

--- src/auth/authenticate.js
+++ src/auth/authenticate.js
@@ -1,5 +1,21 @@
+import util from 'util';
+import crypto from 'crypto';
 import { BasicStrategy } from 'passport-http';
+import orm from 'app/orm';
+import { AuthenticationError } from './errors';
 
-export const basic = new BasicStrategy(function (username, password, done) {
-	done(null, { 'email': username });
+export const basic = new BasicStrategy(async function (username, password, done) {
+	const user = await orm.model('User').findOne({ username });
+
+	if (!user) {
+		return done(new AuthenticationError('Invalid credentials supplied.'));
+	}
+
+	const derivedKey = await util.promisify(crypto.pbkdf2)(password, user.salt, 100000, 128, 'sha512');
+	const isValidPassword = Buffer.compare(user.password, derivedKey) === 0;
+	if (!isValidPassword) {
+		return done(new AuthenticationError('Invalid credentials supplied.'));
+	}
+
+	done(null, user);
 });
--- test/auth/authenticate.tests.js
+++ test/auth/authenticate.tests.js
@@ -1,9 +1,18 @@
 import passport from 'passport';
 import { mockReq, mockRes } from 'sinon-express-mock';
+import factory from 'factory-girl';
+import { AuthenticationError } from 'app/auth/errors';
 
 describe('Authentication', function () {
 	before(async function () {
 		await import('app/auth');
+
+		this.username = factory.chance('email')();
+		this.password = factory.chance('word', { length: 12 })();
+		this.testUser = await factory.create('User', {
+			username: this.username,
+			password: this.password
+		});
 	});
 
 	beforeEach(function () {
@@ -21,22 +30,37 @@ describe('Authentication', function () {
 	describe('Strategies', function () {
 		describe('basic', function () {
 			it('should succesfully authenticate valid username/password pairs', function (done) {
-				this.req.headers = { authorization: `Basic ${Buffer.from('test.user@domain.com:testpassword').toString('base64')}` };
+				this.req.headers = { authorization: `Basic ${Buffer.from(this.username + ':' + this.password).toString('base64')}` };
 				passport.authenticate('basic', (err, user) => {
 					if (err) {
 						done(err);
 					};
 
-					user.email.should.equal('test.user@domain.com');
+					user.username.should.equal(this.username);
 
 					done();
 				})(this.req, this.res, this.next);
 			});
 
-			it('should succesfully error invalid username/password pairs', function (done) {
-				this.req.headers = { authorization: `Basic ${Buffer.from('test.user@domain.com:badpassword').toString('base64')}` };
+			it('should gracefully error given invalid usernames', function (done) {
+				this.req.headers = { authorization: `Basic ${Buffer.from('test.user@domain.com:' + this.password).toString('base64')}` };
 				passport.authenticate('basic', err => {
+
 					err.should.exist;
+					err.should.be.an.instanceOf(AuthenticationError);
+					done();
+
+				})(this.req, this.res, this.next);
+			});
+
+			it('should gracefully error given invalid passwords', function (done) {
+				this.req.headers = { authorization: `Basic ${Buffer.from(this.username + ':badpassword').toString('base64')}` };
+				passport.authenticate('basic', err => {
+
+					err.should.exist;
+					err.should.be.an.instanceOf(AuthenticationError);
+					done();
+
 				})(this.req, this.res, this.next);
 			});
 		});
