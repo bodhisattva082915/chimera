735e54b899a1a3dd3dbbe8cfaadb1891b9e031b4
Andrew Bettke <14064130+abettke@users.noreply.github.com>
2019-04-11 11:00:22 +1200

    Invert hex gradient effect to top corner.

--- assets/auth/views/index.js
+++ assets/auth/views/index.js
@@ -4,36 +4,56 @@ import HexPattern from 'chimera-ui/global/components/HexPattern';
 import Routes from './routes';
 
 const styles = theme => ({
+	bottomCorner: {
+		bottom: 0,
+		left: 0
+	},
+	topCorner: {
+		top: 0,
+		right: 0
+	},
 	hexPattern: {
 		position: 'fixed',
-		height: '50vh',
-		width: '50vw',
-		bottom: 0,
-		left: 0,
+		height: '60vh',
+		width: '60vw',
 		zIndex: -10
 	},
-	gradient: {
+	overlay: {
 		position: 'absolute',
 		right: 0,
 		bottom: 0,
 		left: 0,
 		top: 0,
 		height: '100%',
-		width: '100%',
-		background: `linear-gradient(to top right,  rgba(255,255,255,0), rgba(255,255,255,1) 70%)`
+		width: '100%'
+	},
+	bottomGradient: {
+		background: `linear-gradient(to top right,  rgba(255,255,255,0), rgba(255,255,255,1) 60%)`
+	},
+	topGradient: {
+		background: `linear-gradient(to bottom left,  rgba(255,255,255,0), rgba(255,255,255,1) 60%)`
 	}
 });
 
 class AppContainer extends React.Component {
 	render () {
 		const { classes } = this.props;
+
+		function HexGradient (props) {
+			const { direction } = props; /* eslint-disable-line react/prop-types */
+			return (
+				<div className={`${classes.hexPattern} ${classes[direction + 'Corner']}`}>
+					<HexPattern />
+					<div className={`${classes.overlay} ${classes[direction + 'Gradient']}`}></div>
+				</div>
+			);
+		}
+
 		return (
 			<React.Fragment>
 				<Routes />
-				<div className={classes.hexPattern}>
-					<HexPattern />
-					<div className={classes.gradient}></div>
-				</div>
+				<HexGradient direction={'top'} />
+				<HexGradient direction={'bottom'} />
 			</React.Fragment>
 
 		);
