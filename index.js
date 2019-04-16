f067078686dbc6f8430a07d13a4bd2f98e125067
Andrew Bettke <14064130+abettke@users.noreply.github.com>
2019-04-16 21:27:08 +1200

    Refactor to use mongo-express as express middleware instead of container.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const proxy = require('express-http-proxy');
 const app = require('./dist/main.js');
 
 if (process.env.NODE_ENV === 'development') {
-	app.use('/mongo-admin', proxy('localhost:8081'));
+	const mongoExpress = require('mongo-express/lib/middleware');
+	const config = require('mongo-express/config.default');
+
+	config.mongodb.admin = true;
+	config.mongodb.adminUsername = 'root';
+	config.mongodb.adminPassword = 'root';
+	config.basicAuth.username = 'chimeraAdmin';
+	config.basicAuth.password = 'chimeraAdmin';
+
+	app.use('/mongo-express', mongoExpress(config));
 }
 
 app.listen(8000, err => {
