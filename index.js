a19f1235ec53500701e839a3a4c99a271ac55887
Andrew Bettke <14064130+abettke@users.noreply.github.com>
2019-04-16 18:38:59 +1200

    Add mongo admin proxy for development convenience.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const proxy = require('express-http-proxy');
 const app = require('./dist/main.js');
 
+if (process.env.NODE_ENV === 'development') {
+	app.use('/mongo-admin', proxy('localhost:8081'));
+}
+
 app.listen(8000, err => {
 	if (err) {
 		console.error(err);
